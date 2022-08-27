22551d8d8a1720c7ace806cd5e49d176a743fae0
Rajat Gawre <62611665+Rajat99-glitch@users.noreply.github.com>
2022-08-27 16:31:28 +0530

    Create SearchController.java

---
+++ SearchController.java
@@ -0,0 +1,26 @@
+package search_application;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/home")
+	public String home()
+	{
+		return "home";
+	}
+	
+	@RequestMapping("/form-search")
+	public RedirectView search(@RequestParam("querybox") String query)
+	{
+		String url = "https://www.google.com/search?q=" +query;
+		RedirectView redirect = new RedirectView();
+		redirect.setUrl(url);
+		return redirect;
+		
+	}
+}
